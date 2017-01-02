56ec5a08fb09edf879421e047c96043ade9dad5b
Scott Cederberg <cederber@gmail.com>
2017-01-01 23:07:03 -0800

    Version that parses CSV from hardcoded value.

--- irv/src/Main.hs
+++ irv/src/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
-import Control.Monad.Trans.Writer.Lazy
-    (Writer, listen, pass, runWriter, tell, writer)
+import Control.Monad.Trans.Writer.Lazy (Writer, runWriter, tell, writer)
+import qualified Data.ByteString.Lazy.Char8 as C (pack)
+import Data.Csv (HasHeader(NoHeader), decode)
 import Data.List (intercalate, sortBy)
+import Data.Text (Text, unpack)
+import qualified Data.Vector as V
 import Safe (headMay)
 
 
@@ -20,12 +23,23 @@ main = do
 
 
 votes :: [Vote]
-votes = [
-    ["a", "b", "c"],
-    ["a", "b", "c"],
-    ["b", "a", "c"],
-    ["b", "a", "c"],
-    ["c", "a", "b"]]
+votes =
+  let dec = decode NoHeader
+        $ C.pack
+          ("a,b,c\n"
+          ++ "a,b,c\n"
+          ++ "b,a,c\n"
+          ++ "b,a,c\n"
+          ++ "c,a,b\n") :: Either String (V.Vector [Text])
+      unpackVotes (Left e) = error e
+      unpackVotes (Right v) = map (map unpack) (V.toList v)
+   in unpackVotes dec
+
+
+vectorToList :: V.Vector a -> [a]
+vectorToList v
+    | null v = []
+    | otherwise = V.head v : vectorToList (V.tail v)
 
 
 findWinner :: [Vote] -> (Option, [String])
@@ -69,8 +83,8 @@ eliminate roundResult = eliminate' (reverse roundResult) [] (-1)
 eliminate' :: [(Int, Option)] -> [Option] -> Int -> [Option]
 eliminate' [] e _ = e
 eliminate' (o:os) e i
-    | fst o < i               = error "Out of order votes"
-    | i == (-1) || fst o == i = eliminate' os (snd o:e) (fst o)
+    | fst o < i                 = error "Out of order votes"
+    | i == (-1) || fst o == i   = eliminate' os (snd o:e) (fst o)
     | otherwise                 = e
 
 
