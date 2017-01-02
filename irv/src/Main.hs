f886cc8e08c18d39fc11006c0decb74b5a489598
Scott Cederberg <cederber@gmail.com>
2017-01-01 17:48:09 -0800

    + Create Stack project. + Use Writer monad to accumulate intermediate output.

---
+++ irv/src/Main.hs
@@ -0,0 +1,89 @@
+module Main where
+
+import Control.Monad.Trans.Writer.Lazy
+    (Writer, listen, pass, runWriter, tell, writer)
+import Data.List (intercalate, sortBy)
+import Safe (headMay)
+
+
+type Option = String
+type Round = Int
+type Vote = [Option]
+
+
+main :: IO ()
+main = do
+  let (winner, output) = findWinner votes
+  putStrLn $ unlines output
+  putStrLn ""
+  putStrLn $ show winner ++ " wins!"
+
+
+votes :: [Vote]
+votes = [
+    ["a", "b", "c"],
+    ["a", "b", "c"],
+    ["b", "a", "c"],
+    ["b", "a", "c"],
+    ["c", "a", "b"]]
+
+
+findWinner :: [Vote] -> (Option, [String])
+findWinner votes = runWriter $ findWinner' votes 1 [] ()
+
+
+findWinner' :: [Vote] -> Round -> [Option] -> () -> Writer [String] Option
+findWinner' votes rnd eliminated _ =
+  let roundResult = countVotes votes eliminated
+      voteLog =
+          [[] | rnd > 0]
+          ++ ["Round #" ++ show rnd]
+          ++ showResult roundResult
+  in if hasWinner roundResult
+       then writer (snd $ head roundResult, voteLog)
+       else
+         let eliminatedThisRound = eliminate roundResult
+             elimLog = ["Eliminate " ++ intercalate "," eliminatedThisRound]
+             newEliminated = eliminated ++ eliminatedThisRound
+         in tell (voteLog ++ elimLog)
+             >>= findWinner' votes (rnd + 1) (eliminated ++ eliminatedThisRound)
+
+
+showResult :: [(Int, Option)] -> [String]
+showResult = map (\r -> show (fst r) ++ "\t" ++ snd r)
+
+
+-- Count votes based on the highest-ranked not-yet-eliminated option.
+-- Return in ordered list.
+countVotes :: [Vote] -> [Option] -> [(Int, Option)]
+countVotes votes eliminated =
+  let firstValid v = headMay $ filter (not . (`elem` eliminated)) v
+      validVotes = map firstValid votes
+  in  sortBy (flip compare) $ foldl countOption [] validVotes
+
+
+eliminate :: [(Int, String)] -> [String]
+eliminate roundResult = eliminate' (reverse roundResult) [] (-1)
+
+
+eliminate' :: [(Int, String)] -> [String] -> Int -> [String]
+eliminate' [] e _ = e
+eliminate' (o:os) e i
+    | fst o < i               = error "Out of order votes"
+    | i == (-1) || fst o == i = eliminate' os (snd o:e) (fst o)
+    | otherwise                 = e
+
+
+hasWinner :: [(Int, Option)] -> Bool
+hasWinner [] = False
+hasWinner roundResult = 2 * most > total
+    where most = fst $ head roundResult
+          total = sum (map fst roundResult)
+
+
+countOption :: [(Int, Option)] -> Maybe Option -> [(Int, Option)]
+countOption counts Nothing = counts
+countOption [] (Just o) = [(1, o)]
+countOption ((n,o1):os) (Just o)
+    | o1 == o   = (n+1, o):os
+    | otherwise = (n, o1):countOption os (Just o)
